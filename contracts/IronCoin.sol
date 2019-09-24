28b672152b9ba4593ee6830c2390ee6e3ad9f262
Guillaume Hivert <guillaume.hivert@outlook.com>
2019-09-24 17:49:35 +0200

    :sparkles: Exercises

--- contracts/IronCoin.sol
+++ contracts/IronCoin.sol
@@ -7,23 +7,12 @@ import "./ownership/Ownable.sol";
 ///  ERC-20 token in order to be used with a Ruby frontend!
 contract IronCoin is Ownable {
   /// @dev Stores the balance for every account.
-  mapping (address => uint) balances;
+  ///   Here !
   /// @dev Stores the allowance for every account.
   ///   Allowance is stored as (SpenderAddress => ValueAllowedToSpent).
-  mapping (address => mapping (address => uint)) withdrawAllowance;
+  ///   Here !
 
   // Define events Transfer and Approval here!
-  event Transfer(
-    address indexed _from,
-    address indexed _to,
-    uint _value
-  );
-
-  event Approval(
-    address indexed _owner,
-    address indexed _spender,
-    uint _value
-  );
 
   string public version; // Version of the contract, following Semver.
   string public name;
@@ -31,50 +20,39 @@ contract IronCoin is Ownable {
   uint8  public decimals;
 
   constructor(
-    string memory _version
+    string _version
   ) Ownable() public {
-    version = _version;
-    name = "IronCoin";
-    symbol = "RBC";
-    decimals = 0;
-    balances[msg.sender] = 1e9;
+    // Do Something here!
   }
 
   /// @dev Returns the total supply of existing token.
   function totalSupply() public pure returns (uint) {
-    return 1e9;
+    // Do Something here!
   }
 
   /// @dev Returns the balance of tokens of someone.
   /// @param _owner The person you want to check balance.
   function balanceOf(address _owner) public view returns (uint) {
-    return balances[_owner];
+    // Do Something here!
   }
 
-  /// @dev Transfer some token to someone else. Should revert if not enough funds. Emit Transfer when done.
+  /// @dev Transfer some token to someone else.
   /// @param _to The receiver of transfer.
   /// @param _value The amount to transfer.
   /// @return True if transfer is successfull, False otherwise.
   function transfer(address _to, uint _value) public returns (bool success) {
-    require(balances[msg.sender] >= _value);
-    balances[msg.sender] -= _value;
-    balances[_to] += _value;
-    emit Transfer(msg.sender, _to, _value);
-    return true;
+    // Do Something here!
   }
 
-  /// @dev Approve a third party to withdraw a certain amount of tokens for the owner. Emit Approval when done.
+  /// @dev Approve a third party to withdraw a certain amount of tokens for the owner.
   /// @param _spender The future approved spender.
   /// @param _value The maximum value the spender will be able to withdraw.
   /// @return True if approving successfull, False otherwise.
   function approve(address _spender, uint _value) public returns (bool success) {
-    withdrawAllowance[msg.sender][_spender] = _value;
-    emit Approval(msg.sender, _spender, _value);
-    return true;
+    // Do Something here!
   }
 
   /// @dev Transfer some tokens form one account to the other if previously approved.
-  ///      Should revert if allowance or balance less than value. Emit Transfer when done.
   /// @param _from The account to send from.
   /// @param _to The account to send to.
   /// @param _value The amount to send.
@@ -83,13 +61,8 @@ contract IronCoin is Ownable {
     address _from,
     address _to,
     uint _value
-  ) public returns (bool success) {
-    require(withdrawAllowance[_from][msg.sender] >= _value && balances[_from] >= _value);
-    withdrawAllowance[_from][msg.sender] -= _value;
-    balances[_to] += _value;
-    balances[_from] -= _value;
-    emit Transfer(_from, _to, _value);
-    return true;
+  ) public onlyOwner returns (bool success) {
+    // Do Something here!
   }
 
   /// @dev Returns the amount a spender can withdraw for another account.
@@ -100,6 +73,5 @@ contract IronCoin is Ownable {
     address _owner,
     address _spender
   ) public view returns (uint remaining) {
-    return withdrawAllowance[_owner][_spender];
-  }
-}
+    // Do something here!
+  }}
